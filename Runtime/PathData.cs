600e620ac360d66a854c085fe526f2092e6fc3da
Pedro Dominicé <pedro.dominice@outlook.com>
2022-04-23 00:37:44 +0200

    V1.0.2 - Add Get and Set Methods

--- Runtime/PathData.cs
+++ Runtime/PathData.cs
@@ -45,7 +45,7 @@ namespace EasyPath
 
 		#endregion
 
-		#region Methods
+		#region Get
 
 		private string GetDirectPath()
 		{
@@ -86,6 +86,82 @@ namespace EasyPath
 				PathSystem.AbsoluteURL => Path.Combine(Application.absoluteURL, prefixPath, path, suffixPath),
 				_ => Path.Combine(prefixPath, GetDirectPath(), suffixPath)
 			};
+		}
+
+		public string GetPartialPath()
+		{
+			return pathSystem switch
+			{
+				PathSystem.GameData => Path.Combine(prefixPath, path, suffixPath, fileName + extension),
+				PathSystem.StreamingAssets => Path.Combine(prefixPath, path, suffixPath, fileName + extension),
+				PathSystem.PersistentData => Path.Combine(prefixPath, path, suffixPath, fileName + extension),
+				PathSystem.TemporaryCache => Path.Combine(prefixPath, path, suffixPath, fileName + extension),
+				PathSystem.Resources => Path.Combine(prefixPath, path, suffixPath, fileName),
+				PathSystem.ConsoleLog => Path.Combine(prefixPath, path, suffixPath, fileName + extension),
+				PathSystem.AbsoluteURL => Path.Combine(prefixPath, path, suffixPath, fileName + extension),
+				_ => Path.Combine(prefixPath, GetDirectPath(), suffixPath, fileName + extension)
+			};
+		}
+
+		public string GetPartialDirectoryPath()
+		{
+			return pathSystem switch
+			{
+				PathSystem.GameData => Path.Combine(prefixPath, path, suffixPath),
+				PathSystem.StreamingAssets => Path.Combine(prefixPath, path, suffixPath),
+				PathSystem.PersistentData => Path.Combine(prefixPath, path, suffixPath),
+				PathSystem.TemporaryCache => Path.Combine(prefixPath, path, suffixPath),
+				PathSystem.Resources => Path.Combine(prefixPath, path, suffixPath),
+				PathSystem.ConsoleLog => Path.Combine(prefixPath, path, suffixPath),
+				PathSystem.AbsoluteURL => Path.Combine(prefixPath, path, suffixPath),
+				_ => Path.Combine(prefixPath, GetDirectPath(), suffixPath)
+			};
+		}
+
+		public string GetSytemPath()
+		{
+			return pathSystem switch
+			{
+				PathSystem.GameData => Application.dataPath,
+				PathSystem.StreamingAssets => Application.streamingAssetsPath,
+				PathSystem.PersistentData => Application.persistentDataPath,
+				PathSystem.TemporaryCache => Application.temporaryCachePath,
+				PathSystem.Resources => string.Empty,
+				PathSystem.ConsoleLog => Application.consoleLogPath,
+				PathSystem.AbsoluteURL => Application.absoluteURL,
+				_ => string.Empty
+			};
+		}
+
+		public string GetFileName()
+		{
+			return fileName + extension;
+		}
+
+		public string GetFileNameWithoutExtension()
+		{
+			return fileName;
+		}
+
+		public string GetExtension()
+		{
+			return extension;
+		}
+
+		#endregion
+
+		#region Set
+
+		public void SetFromPartialPath(string partialPath)
+		{
+			path = Path.GetDirectoryName(partialPath);
+
+			fileName = Path.GetFileName(partialPath);
+
+			if (Path.HasExtension(partialPath))
+			{
+				extension = Path.GetExtension(partialPath);
+			}
 		}
 
 		#endregion
